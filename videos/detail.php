98b68ecc36da226c8730dab1326947bb4b9f83ac
key <lampas-ganibal@outlook.com>
2021-02-24 19:54:03 +0700

    add detail url

--- videos/detail.php
+++ videos/detail.php
@@ -3,9 +3,9 @@ require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Video");
 ?><div>
 	 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.detail",
-	"detail_video",
-	Array(
+	"bitrix:news.detail", 
+	"detail_video", 
+	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"ADD_ELEMENT_CHAIN" => "N",
 		"ADD_SECTIONS_CHAIN" => "Y",
@@ -29,7 +29,10 @@ $APPLICATION->SetTitle("Video");
 		"DISPLAY_TOP_PAGER" => "N",
 		"ELEMENT_CODE" => "",
 		"ELEMENT_ID" => $_REQUEST["id"],
-		"FIELD_CODE" => array(0=>"",1=>"",),
+		"FIELD_CODE" => array(
+			0 => "",
+			1 => "",
+		),
 		"IBLOCK_ID" => "2",
 		"IBLOCK_TYPE" => "videos",
 		"IBLOCK_URL" => "",
@@ -40,9 +43,13 @@ $APPLICATION->SetTitle("Video");
 		"META_KEYWORDS" => "-",
 		"PAGER_BASE_LINK_ENABLE" => "N",
 		"PAGER_SHOW_ALL" => "N",
-		"PAGER_TEMPLATE" => ".default",
+		"PAGER_TEMPLATE" => "bootstrap_v4",
 		"PAGER_TITLE" => "Страница",
-		"PROPERTY_CODE" => array(0=>"video",1=>"Duration",2=>"",),
+		"PROPERTY_CODE" => array(
+			0 => "video",
+			1 => "Duration",
+			2 => "",
+		),
 		"SET_BROWSER_TITLE" => "Y",
 		"SET_CANONICAL_URL" => "N",
 		"SET_LAST_MODIFIED" => "N",
@@ -55,28 +62,38 @@ $APPLICATION->SetTitle("Video");
 		"STRICT_SECTION_CHECK" => "N",
 		"TEMPLATE_THEME" => "blue",
 		"USE_PERMISSIONS" => "N",
-		"USE_SHARE" => "N"
-	)
+		"USE_SHARE" => "N",
+		"PAGER_BASE_LINK" => "/detail.php",
+		"PAGER_PARAMS_NAME" => "arrPager"
+	),
+	false
 );?>
 </div>
 <div>
 	 <?$APPLICATION->IncludeComponent(
-	"bitrix:news.line",
-	"",
-	Array(
+	"bitrix:news.line", 
+	".default", 
+	array(
 		"ACTIVE_DATE_FORMAT" => "d.m.Y",
 		"CACHE_GROUPS" => "Y",
 		"CACHE_TIME" => "300",
 		"CACHE_TYPE" => "A",
-		"DETAIL_URL" => "",
-		"FIELD_CODE" => array("",""),
-		"IBLOCKS" => array("2"),
+		"DETAIL_URL" => "videos/detail.php?id=#ELEMENT_ID#",
+		"FIELD_CODE" => array(
+			0 => "ID",
+			1 => "",
+		),
+		"IBLOCKS" => array(
+			0 => "2",
+		),
 		"IBLOCK_TYPE" => "videos",
 		"NEWS_COUNT" => "20",
 		"SORT_BY1" => "ACTIVE_FROM",
 		"SORT_BY2" => "SORT",
 		"SORT_ORDER1" => "DESC",
-		"SORT_ORDER2" => "ASC"
-	)
+		"SORT_ORDER2" => "ASC",
+		"COMPONENT_TEMPLATE" => ".default"
+	),
+	false
 );?>
 </div><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
